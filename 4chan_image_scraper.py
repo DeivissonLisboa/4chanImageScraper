4472adfade2414a77e56c7cd9c653b3f8f95c1f4
DeivissonLisboa <81440170+DeivissonLisboa@users.noreply.github.com>
2021-11-09 15:37:31 -0300

    Add files via upload

---
+++ 4chan_image_scraper.py
@@ -0,0 +1,43 @@
+import os, sys, requests, json
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+def scraper(threadlink):
+    r = requests.get(threadlink)
+    soup = BeautifulSoup(r.content, "lxml")
+
+    classsubject = soup.find_all("span", {"class": "subject"})[0]
+    threadname = classsubject.string or f'thread {threadlink.split("/")[-1]}'
+    folderpath = os.path.join(os.getcwd(), threadname)
+    os.mkdir(folderpath)
+
+    imgsrc = soup.find_all("a", {"class": "fileThumb"}, href=True)
+    imgsrclist = [img["href"] for img in imgsrc]
+    for src in tqdm(imgsrclist, desc=threadname):
+        imagerequest = requests.get("https:" + src)
+        imagename = src.split("/")[-1]
+        with open(os.path.join(folderpath, imagename), "wb") as image:
+            image.write(imagerequest.content)
+
+
+def help():
+    print(
+        """Usage: ~ python3 4chan_image_scraper.py link thread_link 
+    or ~ python3 4chan_image_scraper.py batch "path/to/batch.json" """
+    )
+
+
+def main():
+    if sys.argv[1] == "link":
+        scraper(sys.argv[2])
+    elif sys.argv[1] == "batch":
+        with open(sys.argv[2], "r") as file:
+            batch = json.load(file)
+            for link in batch["links"]:
+                scraper(link)
+    else:
+        help()
+
+
+main()
