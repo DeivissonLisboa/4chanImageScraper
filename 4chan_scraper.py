d422de9515f1d79e32931dad506ae9f40b08d291
Deivisson Lisboa <dvilaflor@gmail.com>
2023-07-26 02:23:12 -0300

    Refactor code base
    Write better scrape function with renamed variables and new functions
    for better readability

--- 4chan_scraper.py
+++ 4chan_scraper.py
@@ -1,51 +1,80 @@
 import os, sys, requests, json
+import re
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-def scraper(threadlink):
-    r = requests.get(threadlink)
-    soup = BeautifulSoup(r.content, "lxml")
+def getPageHtml(url: str) -> bytes:
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "referer": "https://www.google.com/",
+    }
+    request = requests.get(url, headers=headers)
+    print(request.status_code)
+    return request.content
 
-    classsubject = soup.find_all("span", {"class": "subject"})[0]
-    threadname = (
-        "".join([i for i in classsubject.string if i != "/"])
-        if classsubject.string
-        else f'thread {threadlink.split("/")[-1]}'
-    )
-    folderpath = os.path.join(os.getcwd(), threadname)
+
+def sanitizeString(string: str) -> str:
+    spacesRemoved = re.sub(r"[\s\/]", "_", string)
+    return re.sub(r"[^\w]", "", spacesRemoved)
+
+
+def getThreadTitle(url: str, soup: BeautifulSoup) -> str:
+    thread_board = url.split("org/")[-1]
+
+    subject_span = soup.select_one("span.subject")
+
+    if not subject_span:
+        return sanitizeString(thread_board)
+
+    return sanitizeString(f"{thread_board}_{subject_span.text}")
+
+
+def createFolder(path: str) -> None:
     try:
-        os.mkdir(folderpath)
+        os.mkdir(path)
     except FileExistsError:
         pass
 
-    imgsrc = soup.find_all("a", {"class": "fileThumb"}, href=True)
-    imgsrclist = [img["href"] for img in imgsrc]
-    for src in tqdm(imgsrclist, desc=threadname):
-        imagerequest = requests.get("https:" + src)
-        imagename = src.split("/")[-1]
-        with open(os.path.join(folderpath, imagename), "wb") as image:
-            image.write(imagerequest.content)
+
+def downloadImage(download_path: str, url: str) -> None:
+    # if os.path.exists(download_path):
+    #     return
+
+    source = getPageHtml(url)
+
+    with open(download_path, "wb") as image:
+        image.write(source)
 
 
-def help():
-    print(
-        """Usage: ~ python3 4chan_image_scraper.py link thread_link 
-    or ~ python3 4chan_image_scraper.py batch "path/to/batch.json" """
-    )
+def scrape(url):
+    createFolder("downloads")
+
+    html = getPageHtml(url)
+    soup = BeautifulSoup(html, "lxml")
+
+    thread_title = getThreadTitle(url, soup)
+
+    folder_path = os.path.join("downloads", thread_title)
+    createFolder(folder_path)
+
+    img_anchor = soup.find_all("a", {"class": "fileThumb"}, href=True)
+
+    hrefs = [f"https:{img['href']}" for img in img_anchor]
+
+    print(f"Downloading: {thread_title}")
+    for href in tqdm(hrefs):
+        image_filename = href.split("/")[-1]
+
+        image_download_path = os.path.join(folder_path, image_filename)
+
+        downloadImage(image_download_path, href)
 
 
 def main():
-    if sys.argv[1] == "link":
-        scraper(sys.argv[2])
-    elif sys.argv[1] == "batch":
-        with open(sys.argv[2], "r") as file:
-            batch = json.load(file)
-            for link in batch["links"]:
-                scraper(link)
-    else:
-        help()
+    scrape(input("thread url: "))
 
 
 if __name__ == "__main__":
     main()
+    # 403 status code response for all images href. why???
